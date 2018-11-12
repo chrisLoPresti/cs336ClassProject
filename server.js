9cd51666fc9db323efdff04df9d8b3e9db2e80e0
Chris LoPresti <lopresti.chris@yahoo.com>
2018-11-12 15:48:00 -0500

    added 24 hour code to update db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const schedule = require("node-schedule");
+const axios = require("axios");
 
 //sett up our app
 const app = express();
@@ -22,6 +24,20 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+//every day at 12:01am this call will update our inventory table to match the previous days inventory
+var j = schedule.scheduleJob("1 0 * * *", function() {
+  axios
+    .post(
+      "https://xja36rg9of.execute-api.us-east-1.amazonaws.com/dev/v1/daily/update/inventory"
+    )
+    .then(res => {
+      console.log("Updated database at 12:00am");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 //use the env. server port or localhost:5000
 const port = process.env.PORT || 5000;
