700cec014d695120b1b34937badea1f6ccef57ef
Chris LoPresti <lopresti.chris@yahoo.com>
2018-11-13 15:34:55 -0500

    added query warning about time

--- client/src/components/randomquery/patternqueries.js
+++ client/src/components/randomquery/patternqueries.js
@@ -15,7 +15,7 @@ const queries = [
   {
     title: "No Bars Can Have Transactions When They Are Closed",
     description:
-      "Run this query to make sure that no bars have transactions when they are closed. If true, we will return 1, if false we will return 0",
+      "Run this query to make sure that no bars have transactions when they are closed. If true, we will return 1, if false we will return 0. **This could take a minute or so.",
     req: {
       query:
         "SELECT NOT EXISTS (SELECT * FROM Bills b, Operates o WHERE b.time < o.start AND b.time > o.end) AS no_bills_when_bar_is_closed;"
@@ -24,7 +24,7 @@ const queries = [
   {
     title: "Drinkers Only Frequent Bars In The Same State They Live In",
     description:
-      "Run this query to check to make sure drinkers only frequent bars within the same state they live. If true we will return 1, if not we will return 0",
+      "Run this query to check to make sure drinkers only frequent bars within the same state they live. If true we will return 1, if not we will return 0. **This could take a minute or so.",
     req: {
       query:
         "SELECT NOT EXISTS (SELECT * FROM Frequents f, Bar b, Drinker d WHERE d.name = f.drinker AND f.bar = b.name AND b.state <> d.state) AS No_Out_of_State_Drinkers;"
@@ -33,7 +33,7 @@ const queries = [
   {
     title: "Beer Price Pattern Across Bars",
     description:
-      "For every two beers, b1 and b2, different bars may charge differently for b1 and b2 but b1 should either be less expensive than b2 in ALL bars or more expensive than b2 in ALL bars. If true we will return 1, if not we will return 0",
+      "For every two beers, b1 and b2, different bars may charge differently for b1 and b2 but b1 should either be less expensive than b2 in ALL bars or more expensive than b2 in ALL bars. If true we will return 1, if not we will return 0. **This could take a minute or so.",
     req: {
       query:
         "SELECT NOT EXISTS (SELECT * FROM (SELECT s1.beername AS beer1, s1.barname AS bar, s1.price AS price1, s2.beername AS beer2, s2.price AS price2 FROM SellsBeer s1, SellsBeer s2 WHERE s1.beername <> s2.beername AND s1.barname = s2.barname) s, (SELECT s3.beername AS beer3, s3.barname AS bar, s3.price AS price3,s4.beername AS beer4, s4.price AS price4 FROM SellsBeer s3, SellsBeer s4 WHERE s3.beername <> s4.beername AND s3.barname = s4.barname) s5 WHERE s.bar <> s5.bar AND s.beer1 = s5.beer3 AND s.beer2 = s5.beer4 AND ((s.price1 <= s.price2 AND s5.price3 > s5.price4) OR (s.price1 >= s.price2 AND s5.price3 < s5.price4))) AS comparing_beer_prices;"
@@ -42,7 +42,7 @@ const queries = [
   {
     title: "Bars Cant Sell More Than Whats In Stock",
     description:
-      "Run this query to make sure bars cannot sell more beers of a specific brand than it has in its inventory. If true we will return 1, if not we will return 0",
+      "Run this query to make sure bars cannot sell more beers of a specific brand than it has in its inventory. If true we will return 1, if not we will return 0. **This could take a minute or so.",
     req: {
       query:
         'SELECT NOT EXISTS (SELECT * FROM (SELECT SUM(t.quantity) AS quantity_in_transactions, b1.start_quantity FROM (SELECT b.bill_id AS bill_id, i.beer AS beer, b.bar AS bar, b.date AS date, i.startquantity AS start_quantity FROM Inventory i, Bills b WHERE i.bar = b.bar AND i.date = b.date) b1, Transactions t WHERE t.type = "beer" AND b1.bill_id = t.bill_id AND t.item = b1.beer GROUP BY b1.beer, b1.bar, b1.date) c WHERE c.start_quantity < c.quantity_in_transactions) AS not_selling_more_than_have;'
@@ -51,7 +51,7 @@ const queries = [
   {
     title: "Inventory Must Properly Represent What Has Been Sold",
     description:
-      "Run this query to make sure that our inventory is properly deducted from when we sell an item and that the totals all check out. If true we will return 1, if not we will return 0",
+      "Run this query to make sure that our inventory is properly deducted from when we sell an item and that the totals all check out. If true we will return 1, if not we will return 0. **This could take a minute or so.",
     req: {
       query:
         'SELECT NOT EXISTS (SELECT * FROM (SELECT SUM(t.quantity) AS quantity_in_transactions, (b1.startquantity - b1.endquantity) AS num_sold FROM (SELECT b.bill_id AS bill_id, i.beer AS beer, b.bar AS bar, b.date AS date, i.startquantity AS startquantity, i.endquantity AS endquantity FROM Inventory i, Bills b WHERE i.bar = b.bar AND i.date = b.date) b1, Transactions t WHERE t.type = "beer" AND b1.bill_id = t.bill_id AND t.item = b1.beer GROUP BY b1.beer, b1.bar, b1.date) c WHERE c.num_sold <> c.quantity_in_transactions) AS not_selling_more_than_have;'
@@ -60,7 +60,7 @@ const queries = [
   {
     title: "Bartenders Can Only Work One Shift A Day",
     description:
-      "Run this query to make sure that no bartender has more then one shift a day. If true we will return 1, if not we will return 0",
+      "Run this query to make sure that no bartender has more then one shift a day. If true we will return 1, if not we will return 0. **This could take a minute or so.",
     req: {
       query:
         "SELECT NOT EXISTS( SELECT COUNT(*) AS number_of_shifts_a_day FROM Shifts s GROUP BY s.bartender, s.date HAVING number_of_shifts_a_day <> 1) AS no_bartender_has_more_than_one_shift_a_day;"
